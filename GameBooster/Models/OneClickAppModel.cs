f23333b9e95904741f8f27f4ca854fe1e9daf2cd
Alexandra Nesterova <alex.nesterova@gmail.com>
2013-10-18 17:23:26 +0500

    Game Booster initial commit

---
+++ GameBooster/Models/OneClickAppModel.cs
@@ -0,0 +1,19 @@
+﻿/// <summary>
+/// The <see cref="GameBooster.Models"/> namespace contains a set of model classes
+/// of the <see cref="GameBooster"/> project
+/// </summary>
+namespace GameBooster.Models
+{
+    /// <summary>
+    /// OneClickApp statuses collection
+    /// </summary>
+    public enum OneClickAppStatus
+    {
+        NotStarted,
+        ScanStarted,
+        ScanFinishedOK,
+        ScanFinishedError,
+        FixStarted,
+        FixFinished
+    };
+}
